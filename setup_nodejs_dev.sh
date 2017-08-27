95b37fb228003446df3956ccf752b6beeee001d4
David@T460 <david@shipsfocus.com>
2017-08-27 16:20:49 +0800

    setup script for NodeJS and Angular Development

---
+++ setup_nodejs_dev.sh
@@ -0,0 +1,8 @@
+# Install NodeJS
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo apt-get install -y build-essential
+
+# Angular
+sudo npm install -g @angular/cli@latest
